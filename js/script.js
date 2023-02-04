15617cf21565e8cf93e6f91845bbc5b444bdbec8
Reginaldo750 <107959064+ReginaldoMonteiro@users.noreply.github.com>
2023-02-04 20:09:22 -0300

    Atualizado

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let chave = "cebcd482eda57fa9a6714c1c2ba91885"
+
+function dadosNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em: " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = Math.floor (dados.main.humidity)
+    document.querySelector(".iconeNuvem").src="https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+
+    
+
+}
+
+async function buscarCidade(cidade){
+    let dados = await fetch( "https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=" + chave + "&lang=pt_br" +
+    "&units=metric" ).then(resposta => resposta.json())
+
+    dadosNaTela(dados)
+
+}
+
+
+
+function clickBuscador(){
+    let cidade = document.querySelector(".input-Cidade").value
+
+    buscarCidade(cidade)
+    
+}
