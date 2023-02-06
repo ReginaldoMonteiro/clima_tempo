a9246732d9f8eab58f142a6a353653f08973ad40
Reginaldo750 <107959064+ReginaldoMonteiro@users.noreply.github.com>
2023-02-06 11:49:59 -0300

    up

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function dadosNaTela(dados){
     document.querySelector(".cidade").innerHTML = "Tempo em: " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor (dados.main.temp) + "°C"
     document.querySelector(".descricao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = Math.floor (dados.main.humidity)
+    document.querySelector(".umidade").innerHTML = Math.floor (dados.main.humidity) + "% de umidade"
     document.querySelector(".iconeNuvem").src="https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
 
     
